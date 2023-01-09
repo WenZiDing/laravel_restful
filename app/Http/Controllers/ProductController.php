9a70289b5b9f3623c83750530a4f36bc7a69f707
root <root@localhost.localdomain>
2023-01-09 21:26:29 +0800

    #Laravel Restful Practice

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $product = Product::all();
+        return response($product, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+//        dd($request->all());
+        $input = $request->all();
+        $Product = Product::create([
+            'name' => $input['name'],
+            'price' => $input['price'],
+            'note' => $input['note']
+        ]);
+        return response($Product, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+//        dd($id);
+        $Product = Product::where('id',$id)->get();
+        return response($Product, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $input = $request->all();
+        $product = Product::find($id);
+        $product->fill([
+            'price'=>$input['price'],
+            'note'=>$input['note']
+            ]);
+        $product->save();
+        return response()->json(true);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $product = Product::find($id)->delete();
+        return response()->json(true);
+    }
+}
